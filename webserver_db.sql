c774f0af4d67f5661668da4a64552450707f573d
lkgiustiniani <lkgiustiniani@gmail.com>
2021-02-08 04:46:34 -0500

    dump webserver

---
+++ webserver_db.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA cars; 
+USE cars; 
+CREATE TABLE car_table (marca VARCHAR(100), modello VARCHAR(100)); 
+INSERT INTO car_table (marca, modello) VALUES ('ferrari', '1');
+INSERT INTO car_table (marca, modello) VALUES ('ferrari', '2'); 
+INSERT INTO car_table (marca, modello) VALUES ('BMW', '1');
+INSERT INTO car_table (marca, modello) VALUES ('BMW', '2');
